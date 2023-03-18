3ccdfb53dfff5ca58a5dcef85a9481d0b2ffbc9a
IsiteYves <yvesisite@gmail.com>
2023-03-18 18:11:52 +0200

    feat: added try and catch blocks to handle exceptions for all resolvers

--- resolvers/index.js
+++ resolvers/index.js
@@ -5,44 +5,55 @@ const resolvers = {
   Query: {
     // Define a resolver for the 'populationData' query, which returns all population data
     getPopulationData: async () => {
-      const data = await PopulationData.find();
-      return data;
+      try {
+        const data = await PopulationData.find();
+        return data;
+      } catch (e) {
+        return { message: e?.message };
+      }
     },
   },
   Mutation: {
     addPopulationData: async (_, args) => {
-      const { Country, Year, Area, TotalPopulation } = args;
-      // Create a new population data object using the model and provided arguments
-      const populationData = new PopulationData({
-        Country,
-        Year,
-        Area,
-        TotalPopulation,
-      });
-      await populationData.save();
-      return populationData;
+      try {
+        const { Country, Year, Area, TotalPopulation } = args;
+        // Create a new population data object using the model and provided arguments
+        const populationData = new PopulationData({
+          Country,
+          Year,
+          Area,
+          TotalPopulation,
+        });
+        await populationData.save();
+        return populationData;
+      } catch (e) {
+        return { message: e?.message };
+      }
     },
     updatePopulationData: async (_, args) => {
-      const { _id, Country, Year, Area, TotalPopulation } = args;
-      const updatedPopulationData = await PopulationData.findOneAndUpdate(
-        { _id },
-        { $set: { Country, Year, Area, TotalPopulation } },
-        { new: true }
-      );
-      return updatedPopulationData;
+      try {
+        const { _id, Country, Year, Area, TotalPopulation } = args;
+        const exists = await PopulationData.findById(_id);
+        if (!exists) return { message: `No document with _id '${_id}'.` };
+        const updatedPopulationData = await PopulationData.findOneAndUpdate(
+          { _id },
+          { $set: { Country, Year, Area, TotalPopulation } },
+          { new: true }
+        );
+        return updatedPopulationData;
+      } catch (e) {
+        return { message: e?.message };
+      }
     },
     deletePopulationData: async (_, args) => {
       try {
         const { _id } = args;
         const exists = await PopulationData.findById(_id);
-        if (exists) {
-          await PopulationData.findByIdAndDelete(_id);
-          return {
-            message: `Successfully deleted the data for document with _id '${_id}'`,
-          };
-        } else {
-          return { message: `No document with _id '${_id}'.` };
-        }
+        if (!exists) return { message: `No document with _id '${_id}'.` };
+        await PopulationData.findByIdAndDelete(_id);
+        return {
+          message: `Successfully deleted the data for document with _id '${_id}'`,
+        };
       } catch (e) {
         return { message: e?.message };
       }
